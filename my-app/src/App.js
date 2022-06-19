7c86160059d4889427ae19418ef466e51579eb5c
Victoria <victoriakarenlo@gmail.com>
2022-06-19 02:44:08 -0700

    add maps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Home from './Home';
 import { Routes,Route, BrowserRouter as Router } from 'react-router-dom';
 import About from './Components/About';
+import MyComponent from './Search';
+
+
+
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 function App() {
   return (
@@ -13,6 +19,11 @@ function App() {
         </p>
 
       </header>
+      <Home></Home>
+
+      <br></br>
+      <Marker></Marker>
+      <MyComponent></MyComponent>
     </div>
   );
 }
