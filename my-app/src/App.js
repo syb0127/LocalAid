76099057ff8496bea46b16afc6c2ceece1a56d9d
Diana Seo <syb0127@berkeley.edu>
2022-06-19 04:44:05 -0700

    created homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import Home from './Home';
-import { Routes,Route, BrowserRouter as Router } from 'react-router-dom';
-import About from './Components/About';
+import React from "react"
+import { Box, Text, Button, ButtonGroup, Stack, Wrap, WrapItem } from "@chakra-ui/react"
 
-function App() {
+export default function App(props) {
   return (
-    
-<div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to Our Application
-        </p>
-
-      </header>
-    </div>
-  );
+    <Box 
+    display='flex'
+    alignItems='center'
+    justifyContent='center'
+    bgColor='teal'
+    height='max'>
+      <Stack spacing={4} direction='col' align='right'>
+          <ButtonGroup
+          >
+            <Wrap >
+              <WrapItem> 
+                <Button colorScheme='teal' size = 'md'>About</Button>
+                <Button colorScheme='teal' size = 'md'>Share</Button>
+                <Button colorScheme='teal' size = 'md'>Search</Button>
+                <Button colorScheme='teal' size = 'md'>Resources</Button>
+              </WrapItem> 
+            </Wrap>
+        </ButtonGroup>
+      </Stack>
+    <Box>
+      <Text fontSize="lg" fontWeight="bold" >
+          Welcome! We are LocalAid :)
+        </Text>
+    </Box>
+    </Box>
+  )
 }
-
-export default App;
